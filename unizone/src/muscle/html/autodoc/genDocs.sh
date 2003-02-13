3b6a010a5fb10e98f5748cda4031c8838ef273fc
Mika Lindqvist <mtl1979@users.noreply.github.com>
2003-02-13 19:37:29 +0000

    Removed redundant files.

--- unizone/src/muscle/html/autodoc/genDocs.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-echo "Generating doxygen documentation.  "
-echo "You will need doxygen installed in order for"
-echo "this script to work.  The BeOS version is available"
-echo "at www.bebits.com, if you need it... Unix version is"
-echo "also available."
-echo ""
-doxygen muscle.dox
-echo "Done!"
