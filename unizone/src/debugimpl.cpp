cd122465374d6667197189b8ee188e99228119fd
Mika Lindqvist <mtl1979@users.noreply.github.com>
2005-01-24 15:44:38 +0000

    Lots of internal designing. Added few more commands.

---
+++ unizone/src/debugimpl.cpp
@@ -0,0 +1,66 @@
+#include "debugimpl.h"
+
+#if !defined(_DEBUG) && !defined(BETA)
+#error "Only include for debug build!"
+#endif
+
+#include <qmultilineedit.h>
+#include <qthread.h>
+#include <qapp.h>
+#include <qmessagebox.h>
+#include <qstring.h>
+#include <qfile.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+FILE * nfp = NULL;
+
+void PRINT(const char *fmt, ...)
+{
+	va_list a;
+	va_start(a, fmt);
+	vfprintf(nfp, fmt, a);
+}
+
+void WPopup(const QString &msg)
+{
+	(void) QMessageBox::information(NULL, QObject::tr( "Unizone (English)" ), msg, QMessageBox::Ok | QMessageBox::Default, QMessageBox::NoButton, QMessageBox::NoButton); 
+}
+
+void WPopup(const char *msg)
+{ 
+	WPopup(QObject::tr(msg));
+}
+
+void
+RedirectDebugOutput()
+{
+	nfp = fopen("stdout.txt", "w");
+	setbuf(nfp, NULL);
+}
+
+void
+CleanupDebug()
+{
+	fclose(nfp);
+}
+
+void
+WAssert(bool test, const char *message, int line, const char *file, const char *date)
+{
+	if (!test) 
+	{ 
+		QString out = QObject::tr("Send this message to postmaster@raasu.org! This message has also been dumped to 'assert.txt'." 
+			"\n\n%1\n\nLine %2\nFile %3\nDate: %4").arg(QObject::tr(message)).arg(line).arg(file).arg(date); 
+		QFile f("assert.txt");
+		if (f.open(IO_WriteOnly)) 
+		{ 
+			QCString tmp = out.local8Bit();
+			f.writeBlock(tmp, tmp.length()); 
+			f.close(); 
+		} 
+		WPopup(out);
+	}
+}
