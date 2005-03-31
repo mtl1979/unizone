69b53a87a93230ba08948f15b5bde381963c5e4c
Mika Lindqvist <mtl1979@users.noreply.github.com>
2005-03-31 03:35:58 +0000

    A lot of code cleanups.

--- unizone/src/wdownloadevent.h
+++ unizone/src/wdownloadevent.h
@@ -25,21 +25,21 @@ public:
 
 	enum
 	{
-		ConnectFailed = 'dEcF',	// sent when the connection failed
+		ConnectFailed = 'dEcF', // sent when the connection failed
 		ConnectInProgress,		// the connection is being established
 		Connected,				// session was connected, negotiating the connection with the remote client
 		Disconnected,			// we got disconnected from the peer before the file was completed
 		FileDone,				// file completed
-		FileFailed,				// file dl failed due to an error (not a disconnection)
+		FileFailed, 			// file dl failed due to an error (not a disconnection)
 		FileStarted,			// started new file dl
-        FileError,				// critical error, file system error
+		FileError,				// critical error, file system error
 		FileDataReceived,		// received some data
 		FileHashing,			// we're md5'ing the file
-		ConnectBackRequest,		// the thread is accepting on a port, send a connect back request to the remote client
-		FileQueued,				// we're queued
+		ConnectBackRequest, 	// the thread is accepting on a port, send a connect back request to the remote client
+		FileQueued, 			// we're queued
 		Init,					// is sent to the GUI with the path of the file being downloaded/requested. This filename may change (also may include the user as well)
 		UpdateUI,				// this is sent to the GUI with a new session ID/name pair to update for the upload
-		FileBlocked				// user has been banned for file transfers
+		FileBlocked 			// user has been banned for file transfers
 	};
 
 private:
