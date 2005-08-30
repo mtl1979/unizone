735875cca55f1e654b3c09185321ffc5de38e6c4
Mika Lindqvist <mtl1979@users.noreply.github.com>
2005-08-30 15:44:43 +0000

    Fixed parsing bug in search dialog. Bumped to build 9.

--- unizone/src/search.cpp
+++ unizone/src/search.cpp
@@ -412,7 +412,11 @@ WSearch::SplitQuery(const QString &fileExp)
 			// User ID?
 			QString user(uref()->GetUserID());
 			user.prepend("@");
+#if (QT_VERSION >= 0x030000)
+			if (fileExp.endsWith(user))
+#else
 			if (fileExp.right(user.length()) == user)
+#endif
 			{
 				return fileExp.findRev(user);
 			}
@@ -423,9 +427,13 @@ WSearch::SplitQuery(const QString &fileExp)
 			
 			// Compare end of fileExp against stripped user name
 			
+#if (QT_VERSION >= 0x030200)
+			if (fileExp.endsWith(name, false))
+#else
 			if (fileExp.right(name.length()).lower() == name)
+#endif
 			{
-				return fileExp.findRev(name);
+				return fileExp.length() - name.length();
 			}
 		}
 	}
