4da60b89be1056e8097714c3ee46136f0a47c8da
Mika Lindqvist <mtl1979@users.noreply.github.com>
2008-09-03 13:13:40 +0000

    Update translations.

--- isplitter/src/isplitter_en.ts
+++ isplitter/src/isplitter_en.ts
@@ -1,8 +1,10 @@
-<!DOCTYPE TS><TS>
-<codec>utf-8</codec>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="en">
+<defaultcodec>utf-8</defaultcodec>
 <context>
     <name>ImageSplitter</name>
     <message>
+        <location filename="mainwindowimpl.cpp" line="273"/>
         <source>Image Splitter</source>
         <translation>Image Splitter</translation>
     </message>
@@ -10,105 +12,130 @@
 <context>
     <name>ImageSplitterBase</name>
     <message>
+        <location filename="mainwindow.ui" line="34"/>
         <source>Image Splitter</source>
-        <translation type='obsolete'>Image Splitter</translation>
+        <translation>Image Splitter</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="195"/>
         <source>Collage Size:</source>
-        <translation type='obsolete'>Collage Size:</translation>
+        <translation>Collage Size:</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="706"/>
         <source>X</source>
-        <translation type='obsolete'>X</translation>
+        <translation>X</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="804"/>
         <source>0</source>
-        <translation type='obsolete'>0</translation>
+        <translation>0</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="738"/>
         <source>Y</source>
-        <translation type='obsolete'>Y</translation>
+        <translation>Y</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="573"/>
         <source>Image Offset:</source>
-        <translation type='obsolete'>Image Offset:</translation>
+        <translation>Image Offset:</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="664"/>
         <source>Top</source>
-        <translation type='obsolete'>Top</translation>
+        <translation>Top</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="719"/>
         <source>Bottom</source>
-        <translation type='obsolete'>Bottom</translation>
+        <translation>Bottom</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="521"/>
         <source>Image Index:</source>
-        <translation type='obsolete'>Image Index:</translation>
+        <translation>Image Index:</translation>
     </message>
     <message>
+        <location filename="" line="778858605"/>
         <source>Preview</source>
-        <translation type='obsolete'>Preview</translation>
+        <translation type="obsolete">Preview</translation>
     </message>
     <message>
+        <location filename="" line="778858605"/>
         <source>Save</source>
-        <translation type='obsolete'>Save</translation>
+        <translation type="obsolete">Save</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="234"/>
         <source>Collage Offset:</source>
-        <translation type='obsolete'>Collage Offset:</translation>
+        <translation>Collage Offset:</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="99"/>
         <source>Collage</source>
-        <translation type='obsolete'>Collage</translation>
+        <translation>Collage</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="438"/>
         <source>Image</source>
-        <translation type='obsolete'>Image</translation>
+        <translation>Image</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="859"/>
         <source>Rotate</source>
-        <translation type='obsolete'>Rotate</translation>
+        <translation>Rotate</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="783"/>
         <source>Transform</source>
-        <translation type='obsolete'>Transform</translation>
+        <translation>Transform</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="817"/>
         <source>%</source>
-        <translation type='obsolete'>%</translation>
+        <translation>%</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="827"/>
         <source>100</source>
-        <translation type='obsolete'>100</translation>
+        <translation>100</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="840"/>
         <source>Scale</source>
-        <translation type='obsolete'>Scale</translation>
+        <translation>Scale</translation>
     </message>
 </context>
 <context>
     <name>MenuBar</name>
     <message>
+        <location filename="menubar.cpp" line="13"/>
         <source>&amp;Open</source>
         <translation>&amp;Open</translation>
     </message>
     <message>
+        <location filename="menubar.cpp" line="13"/>
         <source>CTRL+O</source>
         <translation>CTRL+O</translation>
     </message>
     <message>
+        <location filename="menubar.cpp" line="14"/>
         <source>&amp;Close</source>
         <translation>&amp;Close</translation>
     </message>
     <message>
+        <location filename="menubar.cpp" line="18"/>
         <source>E&amp;xit</source>
         <translation>E&amp;xit</translation>
     </message>
     <message>
+        <location filename="menubar.cpp" line="18"/>
         <source>ALT+X</source>
         <translation>ALT+X</translation>
     </message>
     <message>
+        <location filename="menubar.cpp" line="21"/>
         <source>&amp;File</source>
         <translation>&amp;File</translation>
     </message>
@@ -116,18 +143,22 @@
 <context>
     <name>Preview</name>
     <message>
+        <location filename="previewimpl.cpp" line="469"/>
         <source>Preview</source>
         <translation>Preview</translation>
     </message>
     <message>
+        <location filename="previewimpl.cpp" line="448"/>
         <source>Save</source>
         <translation>Save</translation>
     </message>
     <message>
+        <location filename="previewimpl.cpp" line="348"/>
         <source>%1 x %2</source>
         <translation>%1 x %2</translation>
     </message>
     <message>
+        <location filename="previewimpl.cpp" line="448"/>
         <source>Unable to save output!</source>
         <translation>Unable to save output!</translation>
     </message>
