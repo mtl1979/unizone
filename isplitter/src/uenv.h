a1de9d10f2d66d39c7072c242e137ac81dc8db83
Mika Lindqvist <postmaster@raasu.org>
2015-06-08 19:21:35 +0300

    Move Image Splitter as own module.

--- isplitter/src/uenv.h
+++
@@ -1,9 +0,0 @@
-#ifndef UENV_H
-#define UENV_H
-
-class QString;
-
-QString EnvironmentVariable(const QString &name);
-
-#endif
- 
--- isplitter/src/util.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-#include <qstring.h>
-
-QString MakePath(const QString &dir, const QString &file);
-
-#endif
