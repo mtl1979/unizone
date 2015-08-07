96153ed7044bdbd820bedc2a8f2f1ed75c0b493d
Mika Lindqvist <postmaster@raasu.org>
2015-08-07 03:34:45 +0300

    Fix warnings about printf formatting under VS2015 * Use "%I64i" instead of "%Li" for 64-bit signed integers

--- tools/cfixname/main.cpp
+++ tools/cfixname/main.cpp
@@ -73,7 +73,11 @@ int main(int argc, char* argv[])
 		}
 		printf("Read %u bytes.\n", sz);
 
+#ifdef WIN32
+		printf("Wrote %I64i bytes.\n", numbytes);
+#else
 		printf("Wrote %Li bytes.\n", numbytes);
+#endif
 		ifile.close();
 		ofile.close();
 		if (sz == numbytes)
