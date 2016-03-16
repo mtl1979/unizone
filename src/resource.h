07b2192431f0e8f4daa29780b12d580fdce37ab2
Mika Lindqvist <postmaster@raasu.org>
2016-03-16 23:03:50 +0200

    Cleanup resource files.

--- src/resource.h
+++ src/resource.h
@@ -3,12 +3,19 @@
 // Used by icon.rc
 //
 #define IDI_ICON1                       101
+#define LANG_FINNISH                    0x040b
+#define SUBLANG_DEFAULT                 0x01
+#define VS_VERSION_INFO                 1
+#define VS_FFI_FILEFLAGSMASK            0x3fL
+#define VOS_NT_WINDOWS32                0x40004L
+#define VFT_APP                         0x1L
+#define VFT2_UNKNOWN                    0x0L
 
 // Next default values for new objects
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
